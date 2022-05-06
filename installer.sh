13203c6fb3507aecee9dae1f52fe9759ce0bf461
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 00:08:24 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/feeds-finder/main/installer.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+#!/bin/sh
+echo
+
+opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
