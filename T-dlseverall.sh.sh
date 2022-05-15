7138c722b286d712cfd5f4cfd9a660b8617a72df
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-15 22:44:56 +0200

    Add files via upload

---
+++ T-dlseverall.sh.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo
+wait
+rm -rf /etc/enigma2/jediplaylists/playlists.txt
+wait
+rm -rf /etc/tuxbox/config/CCcam.cfg
+wait
+rm -rf /etc/tuxbox/config/ncam.server
+wait
+rm -rf /etc/tuxbox/config/oscam.server
+wait
+rm -rf /etc/enigma2/xstreamity/playlists.txt
+wait
+rm -rf /etc/enigma2/iptosat.conf
+wait
+rm -rf /etc/enigma2/ipaudio.json
+wait
+rm -rf /etc/enigma2/xstreamity/playlists.txtwait
+wait
+rm -f /etc/enigma2/playlists
+wait
+rm -f /etc/enigma2/playlist.e2pls
+wait
+rm -f /etc/tsiplayer_xtream.conf
+wait
+sleep 2;
+exit
+
+
+
+
