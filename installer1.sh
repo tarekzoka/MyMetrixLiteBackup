e18f2ef4ca72a40645d86cd087e27673f65ce926
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-29 04:01:36 +0200

    Add files via upload

---
+++ installer1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/plugins/main/installer1.sh -O - | /bin/sh
+###########################################
+###########################################
+#!/bin/sh
+echo
+opkg install --force-overwrite  https://github.com/tarekzoka/plugins/blob/main/jedimakerxtream.tar.gz?raw=true
+wait
+opkg remove --force-depends etc-qaz 
+wait
+tar xzvpf /tmp/*.tar.gz  -C /
+wait
+exit
+
+
+
