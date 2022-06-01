33a926636728c715cd8173924a4f0dd82c922ef0
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-06-01 11:44:14 +0200

    Add files via upload

--- installer6.sh
+++ installer6.sh
@@ -6,9 +6,9 @@
 ###########################################plugins
 #!/bin/sh
 echo
-opkg remove etc-enigma2-lamedb 
+init 4 && sleep 10 && rm -f /etc/defaultsat.tar.gz && rm -f /etc/enigma2/lamedb && rm -f /etc/enigma2/*.tv && rm -f /etc/enigma2/*.radio && rm -f /etc/enigma2/*.del && rm -f /etc/enigma2/*.sh && init 3
 wait
-opkg install --force-overwrite  https://github.com/tarekzoka/plugins/blob/main/plugin-enigma2-channel-tarek_1.1_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tarekzoka/plugins/blob/main/plugin-enigma2-channel-tarek_1.3_all.ipk?raw=true
 wait
 exit
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
