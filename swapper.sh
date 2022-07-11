db49809b50e44379d60abdcee9282f27cfc41ee5
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-07-11 13:38:59 +0200

    Update swapper.sh

--- swapper.sh
+++ swapper.sh
@@ -9,29 +9,21 @@ echo
 ###########################################
 ###########################################
 wget -O /tmp/swapper-nature3.tar.gz "https://github.com/tarekzoka/plugins/blob/main/swapper-nature3.tar.gz?raw=true"
-tar -xzf /tmp/*.tar.gz -C /
-wait
-rm -r /tmp/swapper-nature3.tar.gz
-killall -9 enigma2
-###########################################
-###########################################
-exit 0
-"##############################################################"
-echo ""
-# Download and install bootlogos
+tar -xzf /tmp/swapper-nature3.tar.gz -C /
+# Download and install plugin
 cd /tmp
 set -e
-echo "===> Downloading And Installing  bootlogos Please Wait ......"
+echo "===> Downloading And Installing  plugin Please Wait ......"
 echo
 tar -xzf swapper-nature3.tar.gz -C /
 set +e
 rm -f swapper-nature3.tar.gz
-
+killall -9 enigma2
 echo ""
 sync
 echo "##############################################################"
-echo "#         BOOTLOGOS INSTALLED SUCCESSFULLY             #"
-echo "#             UPLOADED BY TAREK HANFY                #"
+echo "#         PLUGIN INSTALLED SUCCESSFULLY             #"
+echo "#             UPLOADED ............                #"
 echo "##############################################################"
 echo "**************************************************************"
 echo "##############################################################"
@@ -39,6 +31,7 @@ echo "#              RESTART YOUR STB NOW                  #"
 echo "##############################################################"
 
 exit 0
+
 
 
 
