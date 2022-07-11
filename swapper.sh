1dafa0e7b125cf1ce95717ed52c417d1d98c1b8d
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-07-11 02:48:29 +0200

    Update swapper.sh

--- swapper.sh
+++ swapper.sh
@@ -6,17 +6,11 @@
 #!/bin/sh
 echo
 wget -O /tmp/swapper-nature3.tar.gz "https://onedrive.live.com/download?cid=CFCA224FBA296C58&resid=CFCA224FBA296C58%21178&authkey=AO9dpAkA-wjZjp8"
-
 tar -xzf /tmp/*.tar.gz -C /
-
 wait
-
 rm -r /tmp/swapper-nature3.tar.gz
-
 killall -9 enigma2
-
 exit 0
-
 "##############################################################"
 echo ""
 # Download and install bootlogos
