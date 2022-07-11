369cf91125f8706bf88a3777d0dc5f14ad538e4b
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-07-11 02:30:03 +0200

    Add files via upload

---
+++ swapper.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/
+plugins/main/swapper.sh -O - | /bin/sh
+###########################################
+###########################################
+#!/bin/sh
+
+
+
+
+wget -O /tmp/swapper-nature3.tar.gz "https://onedrive.live.com/download?cid=CFCA224FBA296C58&resid=CFCA224FBA296C58%21178&authkey=AO9dpAkA-wjZjp8"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+wait
+
+rm -r /tmp/swapper-nature3.tar.gz
+
+
+
+exit 0
+"##############################################################"
+echo ""
+# Download and install bootlogos
+cd /tmp
+set -e
+echo "===> Downloading And Installing  bootlogos Please Wait ......"
+echo
+tar -xzf swapper-nature3.tar.gz -C /
+set +e
+rm -f swapper-nature3.tar.gz
+
+echo ""
+sync
+echo "##############################################################"
+echo "#         BOOTLOGOS INSTALLED SUCCESSFULLY             #"
+echo "#             UPLOADED BY tarek                #"
+echo "##############################################################"
+echo "**************************************************************"
+echo "##############################################################"
+echo "#              RESTART YOUR STB NOW                  #"
+echo "##############################################################"
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
