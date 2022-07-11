149b6216538389446f986b027e98b3d378fa2062
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-07-11 10:18:00 +0200

    Update swapper.sh

--- swapper.sh
+++ swapper.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 ######################################################################################
 ## Command=wget https://raw.githubusercontent.com/tarekzoka/plugins/main/swapper.sh -O - | /bin/sh
+##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
-
+###########################################
+###########################################
 wget -O /tmp/swapper-nature3.tar.gz "https://github.com/tarekzoka/plugins/blob/main/swapper-nature3.tar.gz?raw=true"
 tar -xzf /tmp/*.tar.gz -C /
 wait
 rm -r /tmp/swapper-nature3.tar.gz
 killall -9 enigma2
-
+###########################################
+###########################################
 exit 0
 "##############################################################"
 echo ""
